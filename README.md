6f65c95e0c01feae4ef5e89de74ec71947d590c3
Drew Hart <drew.ellison.hart@gmail.com>
2021-05-08 22:40:23 -0700

    Adding initial environment.yml and a shell script containing a couple conda commands. Added the environment.yml file with minimum version specifications that I tried to use but couldn't.

---
+++ README.md
@@ -0,0 +1,2 @@
+Files to create a clean, reproducible working environment for
+Python and R using miniconda
---
+++ set_up_conda_env.sh
@@ -0,0 +1,5 @@
+# create env
+conda env create -f environment.yml
+
+# clean up tarballs
+conda clean --all
